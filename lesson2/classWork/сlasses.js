9d77d6ae7c8bbd3e1a157eaf6031efc3df5e82c4
Ivan Nycheporuk <ivan.nycheporuk@gmail.com>
2019-09-03 17:12:43 +0300

    task 2 need refactoring

--- lesson2/classWork/сlasses.js
+++ lesson2/classWork/сlasses.js
@@ -33,8 +33,94 @@
         <button class="post__like">Like!</button>
       </div>
     </div>
-
-
-
-
 */
+
+let postsContainer = document.getElementById('posts_feed');
+
+class Post {
+  constructor(title, image, description) {
+    this.title = title;
+    this.image = image;
+    this.description = description;
+    this.likes = 0;
+
+    this.likePost = this.likePost.bind(this);
+    this.post = document.createElement('div');
+
+    this.render();
+  }
+
+  render() {
+    //let post = document.createElement('div');
+    this.post.classList.add('post');
+
+    this.post.innerHTML = `
+      <div class="post__title">${this.title}</div>
+      <div class="post__image">
+        <img src="${this.image}"/>
+      </div>
+      <div class="post__description">${this.description}</div>
+      <div class="post__footer">
+        <button class="post__like">${this.likes}</button>
+      </div>
+    `;
+
+    this.post.querySelector('.post__like').addEventListener('click', this.likePost);
+
+    return this.post;
+
+    postsContainer.appendChild(this.post);
+  }
+
+  likePost() {
+    this.likes++;
+    this.post.querySelector('.post__like').innerHTML = this.likes;
+  }
+
+}
+
+class Advertisment extends Post {
+  constructor(title, image, description) {
+    super(title, image, description);
+
+    this.buyItem = this.buyItem.bind(this);    
+  }
+
+  render() {
+    this.post.innerHTML = `
+      <div class="post__title">${this.title}</div>
+      <div class="post__image">
+        <img src="${this.image}"/>
+      </div>
+      <div class="post__description">${this.description}</div>
+      <div class="post__footer">
+        <button class="post__like">${this.likes}</button>
+        <button class="post__buy">Buy</button>
+      </div>
+    `;
+
+    this.post.querySelector('.post__like').addEventListener('click', this.likePost);
+
+    this.post.querySelector('.post__buy').addEventListener('click', this.buyItem);
+
+    return this.post;
+    postsContainer.appendChild(this.post);    
+  }
+
+  buyItem() {
+    alert('You have ordered this item, congrats!');
+  }
+}
+
+let feed = [];
+let post = new Post('title', 'https://tinypng.com/images/social/website.jpg', 'some post description');
+let post1 = new Post('title2', 'https://tinypng.com/images/social/website.jpg', 'some post description2');
+let advertisment = new Advertisment('advertisment', 'https://tinypng.com/images/social/website.jpg', 'some advertisment description');
+
+feed.push(post);
+feed.push(post1);
+feed.push(advertisment);
+
+feed.forEach( x => {
+  console.log(x);
+})
